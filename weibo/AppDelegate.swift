dd53b2316b49a507d830190c135040b146eb0b4b
dalufine <dalufine@163.com>
2016-10-13 20:55:48 +0800

    05-(掌握)storyboard reference的使用

--- weibo/AppDelegate.swift
+++ weibo/AppDelegate.swift
@@ -17,12 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //设置全局的颜色
         UITabBar.appearance().tintColor = UIColor.orange
-        //创建window
-        window = UIWindow(frame: UIScreen.main.bounds)
-        //MainViewController直接使用就行，不需要引入
-        window?.rootViewController = MainViewController()
-        window?.makeKeyAndVisible()
-        
+//        //创建window
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        //MainViewController直接使用就行，不需要引入
+//        window?.rootViewController = MainViewController()
+//        window?.makeKeyAndVisible()
+//        
         return true
     }
 
