23b16d0aa0776b3b0c594cb6c0ae2e25ea524aac
dalufine <dalufine@163.com>
2016-10-14 17:55:45 +0800

    16-(掌握)监听注册和登录按钮的点击

--- weibo/AppDelegate.swift
+++ weibo/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //设置全局的颜色
         UITabBar.appearance().tintColor = UIColor.orange
+        
+        UINavigationBar.appearance().tintColor = UIColor.orange
 //        //创建window
 //        window = UIWindow(frame: UIScreen.main.bounds)
 //        //MainViewController直接使用就行，不需要引入
